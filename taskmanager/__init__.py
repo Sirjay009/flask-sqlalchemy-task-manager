e6c726387054724826fe259b1958fe9d8e043241
Sirjay009 <obuttechima@rocketmail.com>
2025-06-25 23:36:23 +0200

    Revert file back to original state

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -4,16 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env # noqa
 
-
 app = Flask(__name__)
-
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-
 db = SQLAlchemy(app)
-
 
 from taskmanager import routes # noqa
