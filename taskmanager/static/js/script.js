28ade46636d6a49a4ec53a156bcb8239cce50994
Sirjay009 <obuttechima@rocketmail.com>
2025-06-26 01:47:56 +0200

    Add in sidebar initialization functionality

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Sidebar Initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
