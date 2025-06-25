fa08d0c5d97fe523b0920c60f2081a985535e300
Sirjay009 <obuttechima@rocketmail.com>
2025-06-25 23:38:26 +0200

    Revert file back to original state

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,11 +4,11 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.Column(
+    category_name = db.Column(
         db.String(24),
         unique=True,
         nullable=False
-        ))
+        )
     tasks = db.relationship(
         "Task", backref="category", cascade="all, delete", lazy=True)
 
