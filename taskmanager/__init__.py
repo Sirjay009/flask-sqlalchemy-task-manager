601558417224ecc252abab76fb6f0d1a8aea6f22
Sirjay009 <obuttechima@rocketmail.com>
2025-06-27 01:41:39 +0200

    Pre-fill form with existing value using the value attribute

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,15 +1,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# Optional env.py import
 if os.path.exists("env.py"):
-    import env # noqa
+        import env  # noqa
+
 
 app = Flask(__name__)
 
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+# Proper PostgreSQL connection string format:
+# postgresql://username:password@localhost:5432/dbname
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+
 db = SQLAlchemy(app)
+
 
 from taskmanager import routes # noqa
