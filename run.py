b466b0403244b92dc5283a67b8c61839705dba9f
Sirjay009 <obuttechima@rocketmail.com>
2025-06-28 05:30:16 +0200

    Update run.py file with create_app()

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os
-from taskmanager import app
+from taskmanager import create_app  # Import the factory function
 
+app = create_app()  # Create the app instance
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=os.environ.get("DEBUG", "True") == "True"
-    )
+        debug=os.environ.get("DEBUG", "False") == "True")
