9961869a6784ef6dd1c00b82c706055ab5936b64
Sirjay009 <obuttechima@rocketmail.com>
2025-06-28 05:01:00 +0200

    Modify database configuration

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,18 +1,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 # Optional env.py import
 if os.path.exists("env.py"):
         import env  # noqa
 
 
+# Initialize Flask app
 app = Flask(__name__)
 
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
+
+# Database configuration
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+         "DB_URL", "sqlite:///taskmanager.db")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
